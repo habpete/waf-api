28b79bdaf00759ceab0ee1ce0656a90622671540
Петр Хабухаев <habpete@mail.ru>
2021-04-11 00:05:44 +0300

    finish kafka client

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module waf
 
 go 1.14
 
-require github.com/segmentio/kafka-go v0.4.13 // indirect
+require (
+	github.com/segmentio/kafka-go v0.4.13
+	gopkg.in/yaml.v2 v2.4.0
+)
--- utils/kafka/kafka-client.go
+++ utils/kafka/kafka-client.go
@@ -1,15 +1,55 @@
 package kafka_client
 
-type kafkaConfig struct {}
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"github.com/segmentio/kafka-go"
+)
+
+var kafkaErrorTemplate = "Kafka read message error: %s"
+
+var pathToKafkaConfig string
+
+func init () {
+	pathToKafkaConfig = "../../values/kafka_values.yaml"
+}
+
+type kafkaConfig struct {
+	Brokers []string
+	TopicName string
+	GroupID string
+}
+
+func (kc *kafkaConfig) Init () error {
+	content, err := ioutil.ReadFile(pathToKafkaConfig)
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(content, kc)
+}
 
 type kafkaClient struct {
 	Client *kafka.Reader
 }
 
-func New () IKafkaClient {
-	return &kafkaClient{}
+func New () (IKafkaClient, error) {
+	cfg := &kafkaConfig{}
+	if err := cfg.Init(); err != nil {
+		return nil, err
+	}
+	return &kafkaClient{
+		Client: kafka.NewReader(kafka.ReaderConfig{
+			Brokers: cfg.Brokers,
+			Topic: cfg.TopicName,
+			GroupID: cfg.GroupID,
+		})
+	}, nil
 }
 
 func (kc *kafkaClient) Consumer (ctx context.Context) interface {
-	return nil
+	msg, err := kc.Client.ReadMessage(ctx)
+	if err != nil {
+		fmt.Errorf(kafkaErrorTemplate, err)
+	}
+	return msg
 }
